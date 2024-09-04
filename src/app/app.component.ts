68a9377a1db4506100a64e3c29e6b70158b649fc
leydimadrid <leydi.madrid6@gmail.com>
2024-09-04 15:57:37 -0500

    sidebar in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MyCounterComponent } from './test-my-counter/my-counter.component';
+import { SideBarComponent } from './Domains/side-bar/side-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
