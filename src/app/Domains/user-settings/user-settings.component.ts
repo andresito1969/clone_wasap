e1bbc39b7fca1f45dc4369af8a310127b10b9fe0
Ernesto A. Rosillo <156781082+andresito1969@users.noreply.github.com>
2024-09-10 22:05:45 +0200

    Develop settings screen, TODO: still nested childs when click + functionalities

---
+++ src/app/Domains/user-settings/user-settings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-user-settings',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './user-settings.component.html',
+  styleUrl: './user-settings.component.css'
+})
+export class UserSettingsComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MyCounterComponent } from './test-my-counter/my-counter.component';
-import { SideBarComponent } from './Domains/';
+import { SideBarComponent, UserSettingsComponent } from './Domains/';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent, SideBarComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent, SideBarComponent, UserSettingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
