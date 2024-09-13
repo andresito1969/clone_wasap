3b728e4296da103e29e4565f5a902ee6872f4298
Ernesto A. Rosillo <156781082+andresito1969@users.noreply.github.com>
2024-09-14 01:01:30 +0200

    feat: initial structure

---
+++ src/app/Domains/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MyCounterComponent } from './test-my-counter/my-counter.component';
 import { SideBarComponent, UserSettingsComponent } from './Domains/';
+import { ProfileComponent } from "./Domains/profile/profile.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent, SideBarComponent, UserSettingsComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, MyCounterComponent, SideBarComponent, UserSettingsComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
