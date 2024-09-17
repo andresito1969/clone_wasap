85f77d95c09f26ceb917be9d7f4e94179d4bdf60
Ernesto A. Rosillo <156781082+andresito1969@users.noreply.github.com>
2024-09-17 20:45:11 +0200

    feat: Router improvement, added route discrimination in profile (show back button when it's called from config)

--- src/app/Domains/user-settings/user-settings.component.ts
+++ src/app/Domains/user-settings/user-settings.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.css'
 })
 export class UserSettingsComponent {
+  constructor(private router: Router){}
 
+  goToProfileFromSettings(){
+    this.router.navigate(['/profile', {returnToConfig: true}])
+  }
 }
